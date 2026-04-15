d675fc4342c00e8b1fc10b0bde2e5242e28c307f
Kamil <alicesun65@gmail.com>
2026-04-15 23:02:41 +0300

    changed base axios url

--- frontend/src/shared/config/env.ts
+++ frontend/src/shared/config/env.ts
@@ -1,3 +1,3 @@
 export const env = {
-    apiBaseUrl: import.meta.env.VITE_API_BASE_URL ?? "http://127.0.0.1:4000",
+    apiBaseUrl: import.meta.env.VITE_API_BASE_URL ?? "http://95.217.209.126:8080",
 }
